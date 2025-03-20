2d830d4abee633b48eae26f1763246a72fea4b85
Nipitphon-Krue <nipitphon2548@gmail.com>
2025-03-20 10:51:25 +0700

    229E-nipitphon-MagnusEffect

---
+++ Assets/Scripts/MagnusEffect.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class MagnusEffect : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] Vector3 velocity, spin;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //kick
+            Kick();
+            //apply magnus effect
+            ApplyMagnusEffect();
+        }
+    }
+    void Kick() 
+    { 
+        rb.linearVelocity = velocity; //เดะตรงไปหน้า
+        rb.angularVelocity = spin; //แรงหมุ่น
+    }
+
+    void ApplyMagnusEffect()
+    {
+        Vector3 magnusForce = Vector3.Cross(rb.linearVelocity , rb.angularVelocity);
+
+        rb.AddForce(magnusForce, ForceMode.Force);
+    }
+}
